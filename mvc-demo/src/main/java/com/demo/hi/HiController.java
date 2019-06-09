5c4e6591b2253a267008eeaac9b5ec0fad2f0008
niqikai <niqikai@aliyun.com>
2019-06-10 00:22:15 +0800

    annotation mvc

---
+++ mvc-demo/src/main/java/com/demo/hi/HiController.java
@@ -0,0 +1,22 @@
+package com.demo.hi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HiController {
+
+
+    @RequestMapping("hi")
+    public ModelAndView sayHello() {
+        System.out.println("sayHello ... ...");
+        ModelAndView mv = new ModelAndView();
+
+        // 设置视图名称
+        mv.setViewName("/WEB-INF/views/welcome.jsp");
+        // 设置共享数据
+        mv.addObject("msg", "hello spring-mvc annotation");
+        return mv;
+    }
+}
