ad6b77a1c371869bf235ce08b0f8b6ecb0f225a1
niqikai <niqikai@aliyun.com>
2019-06-12 23:13:45 +0800

    add interceptor

---
+++ mvc-demo/src/main/java/com/demo/interceptor/CheckLoginInterceptor.java
@@ -0,0 +1,26 @@
+package com.demo.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CheckLoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 没有登陆 return false
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
